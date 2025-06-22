41ebdf20a4f05d947732b6f0a84a4e509d646d75
seanof <sean.oflah@gmail.com>
2025-06-22 17:22:00 +0100

    * Add buffering loading progress spinner for video view. * Add new icon for downloading.

--- app/src/main/java/com/seanof/sakugatomo/ui/SakugaPlayer.kt
+++ app/src/main/java/com/seanof/sakugatomo/ui/SakugaPlayer.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -143,7 +144,7 @@ fun SakugaPlayer(
                                 }
                             ) {
                                 Icon(
-                                    imageVector = Icons.Outlined.KeyboardArrowDown,
+                                    painter = painterResource(id = R.drawable.ic_download),
                                     tint = colorResource(id = R.color.colorTint),
                                     contentDescription = stringResource(R.string.download_icon)
                                 )
