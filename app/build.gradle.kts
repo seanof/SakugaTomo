7ea9faa06678b85afb6c7fc94e07ba2a3873075d
seanof <sean.oflah@gmail.com>
2025-06-22 17:26:12 +0100

    * version 1.0, versionCode 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "com.seanof.sakugatomo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
